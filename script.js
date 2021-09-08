9f198f5c57485ad1fe7103e1bb0ae4bbbcdc0b5a
Jose Ortega <jose.ort98@gmail.com>
2021-09-08 10:50:15 -0500

    ejercicio 1

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //Ejercicios Condicionales
+
+let ejercicio_uno = (num, total) => {
+    let descuento;
+  if (num >= 3) {
+       descuento = total - (total * 0.3);
+  }else{
+      descuento = total - (total * 0.1);
+  }
+  return `El numero de camisasas es ${num}
+  \n Tiene un valor total sin descuento de: ${total}
+  \n Si le aplica el descuento de la promocion le queda en: ${descuento}`;
+}
+console.log(ejercicio_uno(3,100000));
+
+/****************************/
