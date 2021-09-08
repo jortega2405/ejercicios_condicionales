b377a8b2d824d2e7f37d29bba705d8529358fd38
Jose Ortega <jose.ort98@gmail.com>
2021-09-08 11:01:47 -0500

    ejercicio 2

--- script.js
+++ script.js
@@ -7,10 +7,24 @@ let ejercicio_uno = (num, total) => {
   }else{
       descuento = total - (total * 0.1);
   }
-  return `El numero de camisasas es ${num}
+  return `El numero de camisas es ${num}
   \n Tiene un valor total sin descuento de: ${total}
   \n Si le aplica el descuento de la promocion le queda en: ${descuento}`;
 }
 console.log(ejercicio_uno(3,100000));
 
 /****************************/
+
+let ejercicio_dos = (x, total) => {
+    let descuento;
+    if (x < 74) {
+        descuento = (total*0.15);
+    }else{
+        descuento = (total * 0.2);
+    }
+    return `Su descuento es de: ${descuento}`;
+}
+console.log(ejercicio_dos(74, 100))
+
+/****************************/
+
